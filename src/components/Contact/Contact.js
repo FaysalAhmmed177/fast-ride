1f623547d2977cc5af657fddd3df638308bd5291
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-03-20 18:10:26 +0600

    fixed user location dynamically

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <div className="container">
-            <div className="text-center border border-danger w-50">
-                <h1 className="text-primary mt-5">Please contact with us.</h1>
-                <p>Email: faysalahmmed177@gmail.com</p>
-                <p>Mobile: 01618254326</p>
+        <div className="container d-flex justify-content-center mt-5">
+            <div className="text-center bg-danger bg-gray border border-danger rounded w-50">
+                <h1 className="text-dark mt-5">Please contact with us.</h1>
+                <p className="text-white">Email: faysalahmmed177@gmail.com</p>
+                <p className="mb-5 text-white">Mobile: 01618254326</p>
             </div>
         </div>
     );
--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -8,11 +8,22 @@ const Destination = (props) => {
     //const {id} = props.id;
     const { id } = useParams();
     const [click, setClick] = useState(false);
+    const [address, setAddress] = useState({
+        from: '',
+        to: ''
+    });
     // console.log(id);
-    console.log(typeof(id));
+    //console.log(typeof (id));
     const vehicle = fakeData.find(vh => vh.id === parseInt(id));
-    const {name, image, price} = vehicle;
-    console.log("Vehicle", vehicle);
+    const { name, image, price, icon, capacity } = vehicle;
+    //console.log("Vehicle", vehicle);
+
+    const handleBlur = (e)=>{
+     console.log(e.target.name, e.target.value);
+     const newAddress = {...address};
+     newAddress[e.target.name] = e.target.value;
+     setAddress(newAddress);
+    }
 
     const handleSubmit = (e) => {
         setClick(!click)
@@ -22,28 +33,31 @@ const Destination = (props) => {
     return (
 
         <div className="container">
-            <h1>Destination: {id}</h1>
-            <p>Onclick: {click}</p>
-
             <div className="row mt-5">
                 {click ? <div className="col-lg-4 bg-secondary mr-3 rounded  p-3">
                     <div className="bg-warning p-3 rounded">
-                        <h2>Mirpur</h2><br />
-                        <h2>Dhanmondhi</h2>
+                        <h2>{address.from}</h2><br />
+                        <h2>{address.to}</h2>
                     </div>
                     <div className="vehicle-details bg-white d-flex justify-content-center p-3 rounded mt-3">
-                        <img className="vehicle-img ml-2" src={image} alt=""/>
+                        <img className="vehicle-img ml-2" src={image} alt="" />
                         <h3 className="ml-3">{name}</h3>
+                        <img className="ml-3 icon" src={icon} alt="" />
+                        <h3 className="ml-3">{capacity}</h3>
                         <h3 className="ml-3">${price}</h3>
                     </div>
                     <div className="vehicle-details bg-white d-flex justify-content-center p-3 rounded mt-3">
-                        <img className="vehicle-img ml-2" src={image} alt=""/>
+                        <img className="vehicle-img ml-2" src={image} alt="" />
                         <h3 className="ml-3">{name}</h3>
+                        <img className="ml-3 icon" src={icon} alt="" />
+                        <h3 className="ml-3">{capacity}</h3>
                         <h3 className="ml-3">${price}</h3>
                     </div>
                     <div className="vehicle-details bg-white d-flex justify-content-center p-3 rounded mt-3">
-                        <img className="vehicle-img ml-2" src={image} alt=""/>
+                        <img className="vehicle-img ml-2" src={image} alt="" />
                         <h3 className="ml-3">{name}</h3>
+                        <img className="ml-3 icon" src={icon} alt="" />
+                        <h3 className="ml-3">{capacity}</h3>
                         <h3 className="ml-3">${price}</h3>
                     </div>
                 </div> : <div className="col-lg-4 bg-secondary mr-3 rounded  p-3">
@@ -51,16 +65,16 @@ const Destination = (props) => {
 
                         <div className="form-group">
                             <label htmlFor="from" className="text-white">Pick From</label>
-                            <input type="text" name="from" className="form-control" />
+                            <input type="text" name="from" onBlur={handleBlur} className="form-control" required />
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="to" className="text-white">Pick To</label>
-                            <input type="text" name="to" className="form-control" />
+                            <input type="text" name="to" onBlur={handleBlur} className="form-control" required />
                         </div>
 
                         <div className="form-group">
-                            <input type="submit" className="form-control bg-success"  />
+                            <input type="submit" className="form-control bg-success" />
                         </div>
                     </form>
                 </div>}
