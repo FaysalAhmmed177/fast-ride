8aa517f319a7e256e2918862606368fb64ff3aba
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-03-21 18:14:27 +0600

    toggle username with login btn when user login

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-
+import { UserContext } from '../../App';
 import './Header.css';
 
 const Header = () => {
     const history = useHistory();
-    
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     return (
         <div>
             <div className="container">
                 <div className='row pt-4 align-items-center'>
-                    <div className="col-lg-4">
+                    <div className="col-lg-3">
                         <h2>Fast Riders</h2>
                     </div>
-                    <div className="col-lg-8 col-sm-12">
+                    <div className="col-lg-9 col-sm-12">
                         <nav className="nav">
                             <ul>
                                 <li>
@@ -29,7 +29,7 @@ const Header = () => {
                                     <Link to="/contact">Contact</Link>
                                 </li>
                                 <li>
-                                    <Link className="btn btn-primary text-white" to="/login">Login</Link>
+                                    {loggedInUser.name? <a href="#">{loggedInUser.name}</a>: <Link className="btn btn-primary text-white" to="/login">Login</Link>}
                                 </li>
                             </ul>
                         </nav>
